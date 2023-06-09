4fe31c99bee4d7c6544c453c680969b1b16d3890
Seryozha02 <seryozha.asatryan@gmail.com>
2023-06-09 15:53:30 +0400

    Fixing bug for the CV download button

--- src/components/RezumePage.jsx
+++ src/components/RezumePage.jsx
@@ -1,49 +1,90 @@
-import "./RezumePage.css"
-import CV from "../Images/SeryozhaAsatryanCVimg.png"
+import "./RezumePage.css";
+import CV from "../Images/SeryozhaAsatryanCVimg.png";
 
-function RezumePage({changingMode, outsideCloser}) {
+function RezumePage({ changingMode, outsideCloser }) {
+  function cvDownloadPDF() {
+    const fileURL = process.env.PUBLIC_URL + "/SeryozhaAsatryanCV.pdf";
+    const providedTime = new Date().getTime();
 
-    function cvDownloadPDF() {
-        fetch("SeryozhaAsatryanCV.pdf").then(response => {
-            response.blob().then(blob2 => {
-                const fileURL2 = window.URL.createObjectURL(blob2)
-                let blink = document.createElement("a")
-                blink.href = fileURL2
-                blink.download = "SeryozhaAsatryanCV.pdf"
-                blink.click()
-            })
-        })
-    }
+    fetch(`${fileURL}?v=${providedTime}`)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let linkPDF = document.createElement("a");
+        linkPDF.href = fileURL;
+        linkPDF.download = "SeryozhaAsatryanCV.pdf";
+        linkPDF.click();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
+  function cvDownloadPNG() {
+    const fileURL2 = process.env.PUBLIC_URL + "/SeryozhaAsatryanCVimg.png";
+    const providedTime2 = new Date().getTime();
 
-    function cvDownloadPNG() {
-        fetch('SeryozhaAsatryanCVimg.png').then(response => {
-            response.blob().then(blob => {
-                const fileURL = window.URL.createObjectURL(blob);
-                let alink = document.createElement('a');
-                alink.href = fileURL;
-                alink.download = 'SeryozhaAsatryanCVimg.png';
-                alink.click();
-            })
-        })
-    }
+    fetch(`${fileURL2}?v=${providedTime2}`)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const fileURL2 = window.URL.createObjectURL(blob);
+        let linkPNG = document.createElement("a");
+        linkPNG.href = fileURL2;
+        linkPNG.download = "SeryozhaAsatryanCVimg.png";
+        linkPNG.click();
+      })
+      .catch((error2) => {
+        console.log(error2);
+      });
+  }
 
-    return (
-        <div className={changingMode === "light" ? "rezumePage" : "rezumePageDarkMode"} onClick={() => {
-            outsideCloser()
-        }}>
-            <h1 className={changingMode === "light" ? "pageTittle" : "pageTittleDarkMode"}>Rezume</h1>
-            <span className="spanForRezume">Here is my CV, if you are interested, you can download it.</span>
-            <div className="divForRezume">
-                <img className="myCV" src={CV} alt="" />
-            </div>
-            <div className={changingMode === "light" ? "downloadButtons" : "downloadButtonsDarkMode"}>
-                <button className={changingMode === "light" ? "downloadCV" : "downloadCVDarkMode"} onClick={cvDownloadPDF}>Download CV as PDF</button>
-                <p>or</p>
-                <button className={changingMode === "light" ? "downloadCV" : "downloadCVDarkMode"} onClick={cvDownloadPNG}>Download CV as PNG</button>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className={changingMode === "light" ? "rezumePage" : "rezumePageDarkMode"}
+      onClick={() => {
+        outsideCloser();
+      }}
+    >
+      <h1
+        className={
+          changingMode === "light" ? "pageTittle" : "pageTittleDarkMode"
+        }
+      >
+        Rezume
+      </h1>
+      <span className="spanForRezume">
+        Here is my CV, if you are interested, you can download it.
+      </span>
+      <div className="divForRezume">
+        <img className="myCV" src={CV} alt="" />
+      </div>
+      <div
+        className={
+          changingMode === "light"
+            ? "downloadButtons"
+            : "downloadButtonsDarkMode"
+        }
+      >
+        <button
+          className={
+            changingMode === "light" ? "downloadCV" : "downloadCVDarkMode"
+          }
+          onClick={cvDownloadPDF}
+        >
+          Download CV as PDF
+        </button>
+        <p>or</p>
+        <button
+          className={
+            changingMode === "light" ? "downloadCV" : "downloadCVDarkMode"
+          }
+          onClick={cvDownloadPNG}
+        >
+          Download CV as PNG
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default RezumePage;
